aefa0925bc3471f0b419439411bdc61dff1561f8
Javier Diaz <hello@coderdiaz.me>
2019-02-03 01:31:39 -0600

    feat: added new design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation';
 import './App.scss';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App"></div>
+      <div className="App">
+        <header className="header bg-blue-darkest">
+          <div className="container mx-auto">
+            <Navigation></Navigation>
+          </div>
+        </header>
+        <main className="main"></main>
+      </div>
     );
   }
 }
