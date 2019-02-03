8849e4856a7e9678ae51a709b80cd64285059c37
Javier Diaz <hello@coderdiaz.me>
2019-02-03 14:16:04 -0600

    fix: added logic for searchbox focus and defocus

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBox = ({ placeholderText }) => {
-  return (<div className="search-box bg-white flex">
-    <input className="w-full rounded p-2" type="text" placeholder={placeholderText} />
-    <button className="w-auto flex justify-end items-center p-2">
-      <i className="fas fa-search"></i>
-    </button>
-  </div>);
+class SearchBox extends Component {
+  state = {
+    originalWidth: 0,
+  };
+
+  constructor (props) {
+    super(props);
+    this.width = props.width;
+    this.placeholderText = props.placeholderText;
+  }
+
+  componentDidMount () {
+    this.setState({ originalWidth: this.width });
+  }
+
+  onFocus = () => this.setState({ originalWidth: this.width * 2 });
+  deFocus = () => this.setState({ originalWidth: this.width });
+
+  render () {
+    return (<div className="search-box bg-white flex">
+      <input
+        style={{width: this.state.originalWidth + 'px'}}
+        className="search__input rounded p-2"
+        type="text"
+        placeholder={this.placeholderText}
+        onFocus={this.onFocus}
+        onBlur={this.deFocus} />
+      <button className="w-auto flex justify-end items-center p-2">
+        <i className="fas fa-search"></i>
+      </button>
+    </div>);
+  }
 }
 
 export default SearchBox;
