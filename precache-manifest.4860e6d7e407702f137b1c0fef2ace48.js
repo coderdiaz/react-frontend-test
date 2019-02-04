04f9d3e284abc5fa3c00cfc9f74a7a3e3a53ae95
Javier Diaz <hello@coderdiaz.me>
2019-02-03 23:13:30 -0600

    a30d294

---
+++ precache-manifest.4860e6d7e407702f137b1c0fef2ace48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f156a9931a116fbd92b",
+    "url": "/static/js/main.0f156a99.chunk.js"
+  },
+  {
+    "revision": "d08f8978354b63cdfc75",
+    "url": "/static/js/1.d08f8978.chunk.js"
+  },
+  {
+    "revision": "0f156a9931a116fbd92b",
+    "url": "/static/css/main.af3c1291.chunk.css"
+  },
+  {
+    "revision": "23349c19860b53eb7df5e169e81ee7c8",
+    "url": "/index.html"
+  }
+];
